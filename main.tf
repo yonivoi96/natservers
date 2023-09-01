86a8f2f05b648a1f23d9b601d8285f5e1cdf6c21
yonivoi96 <yonatan.voi@gmail.com>
2023-09-01 21:46:50 +0300

    final servers

--- main.tf
+++ main.tf
@@ -8,6 +8,25 @@ resource "aws_vpc" "nats_vpc" {
   enable_dns_hostnames = true
 }
 
+resource "aws_route_table" "nats_route_table" {
+  vpc_id = aws_vpc.nats_vpc.id
+}
+
+resource "aws_internet_gateway" "nats_gateway" {
+  vpc_id = aws_vpc.nats_vpc.id
+}
+
+resource "aws_route" "internet_gateway_route" {
+  route_table_id         = aws_route_table.nats_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.nats_gateway.id
+}
+
+resource "aws_route_table_association" "nats_association" {
+  subnet_id      = aws_subnet.nats_subnet.id
+  route_table_id = aws_route_table.nats_route_table.id
+}
+
 resource "aws_subnet" "nats_subnet" {
   vpc_id            = aws_vpc.nats_vpc.id
   cidr_block        = "10.0.0.0/24"
@@ -30,6 +49,25 @@ resource "aws_security_group" "nats_sg" {
     protocol  = "tcp"
     self      = true
   }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "tf-key-pair" {
+  key_name   = "tf-key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "local_file" "tf-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tf-key-pair"
 }
 
 resource "aws_instance" "nats_server" {
@@ -39,6 +77,7 @@ resource "aws_instance" "nats_server" {
   subnet_id                   = aws_subnet.nats_subnet.id
   security_groups             = [aws_security_group.nats_sg.id]
   associate_public_ip_address = true
+  key_name                    = "tf-key-pair"
 
   tags = {
     Name = "nats-server-${count.index + 1}"
